8b40dbb8e8d76bed437bf6171a584c4d4cddff61
Ramon de Klein <mail@ramondeklein.nl>
2017-01-19 10:51:16 +0100

    Bumped version.

--- DeadlockDetection/Properties/AssemblyInfo.cs
+++ DeadlockDetection/Properties/AssemblyInfo.cs
@@ -1,11 +1,12 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DeadlockDetection")]
 [assembly: AssemblyDescription("Deadlock detection library")]
-[assembly: AssemblyInformationalVersion("0.0.1.0 (beta)")]
+[assembly: AssemblyInformationalVersion("0.0.2.0 (beta)")]
 #if DEBUG
 [assembly: AssemblyConfiguration("DEBUG")]
 #else
@@ -25,5 +26,6 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
+[assembly: CLSCompliant(true)]
